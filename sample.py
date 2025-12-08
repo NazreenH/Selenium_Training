f776447d025eeb6904d1de1505c2720f16bc7df5
NazreenH <nazreen.dh@gmail.com>
2025-12-08 13:01:03 +0530

    defined even_odd() for checking series of nos are even or odd

--- sample.py
+++ sample.py
@@ -1,6 +1,9 @@
 
-for num in range(1,20):
-    if num%2==0:
-        print(f"{num} is even")
-    else:
-        print(f"{num} is odd")
+def even_odd(start,stop):
+    for num in range(start,stop):
+        if num%2==0:
+            print(f"{num} is even")
+        else:
+            print(f"{num} is odd")
+
+even_odd(1,10)
